00b4ab3b2e83e58ea982ec7194ede2f6dc1dd8f9
大川峻 <okawashun@MacBook-Pro.local>
2020-07-06 10:31:27 +0900

    change output style
    Markdown table

--- resources/views/infos/output.blade.php
+++ resources/views/infos/output.blade.php
@@ -5,10 +5,16 @@
     <div class="input">
         @foreach ($decode_res as $items)
             @foreach ($items as $res)
-                <p>{{ $res->title }}</p>
-                <p>{{ $res->updated_at }}</p>
+                @php
+                    $created_at = $res->created_at;
+                    $timestamp = strtotime($created_at);
+                    $datetime = date('Y-m-d H:i:s', $timestamp);
+                @endphp
+
+                <p>| 記事投稿日 | 記事タイトル | 記事リンク |</p>
+                <p>| --- | --- | --- |</p>
+                <p>| {{ $datetime }} | {{ $res->title }} | {{ $res->url }} |</p>
                 <br>
-                <hr>
             @endforeach
         @endforeach
     </div>
